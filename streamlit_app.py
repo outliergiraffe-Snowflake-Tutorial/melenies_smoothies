4b229d41d8a50c0f1082b0e0d4884a0a76254d3f
outliergiraffe-Snowflake-Tutorial <outliergiraffe@gmail.com>
2026-08-31 18:51:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         # smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-        st.text(smoothiefroot_response)
+        st.text(smoothiefroot_response.json())
  
 
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
