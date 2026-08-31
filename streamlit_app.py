527f852df23a3dcfbf1f96f3861ff0cb23231f72
outliergiraffe-Snowflake-Tutorial <outliergiraffe@gmail.com>
2026-08-31 19:50:41 +0530

    Update streamlit_app.py
    Adding nutrition information on home screen

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,7 @@ if ingredients_list:
     st.text(ingredients_list)
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         # smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
         # st.text(smoothiefroot_response.json())
